17603a9c35b882ade044292d46e4aace7e540ecf
eunjinjeong <eunjin.jeong.5115@gmail.com>
2023-12-21 21:09:43 +0900

    중복커밋 해결

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MainScreen());
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MainScreen(),
+    );
   }
 }
