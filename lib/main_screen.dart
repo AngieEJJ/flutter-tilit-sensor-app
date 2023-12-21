239347472f6f783113102845286c6ba638e2fc04
eunjinjeong <eunjin.jeong.5115@gmail.com>
2023-12-22 02:08:38 +0900

    수평계 기능 구현 및 가로 세로 축 추가

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -18,34 +18,47 @@ class _MainScreenState extends State<MainScreen> {
 
     final centerX = MediaQuery.of(context).size.width / 2 - 50;
     final centerY = MediaQuery.of(context).size.height / 2 - 50;
+
     return Scaffold(
-        body: Stack(children: [
-      StreamBuilder<AccelerometerEvent>(
-          //AccelerometerEvent를 받는다.
-          stream:
-              accelerometerEventStream(), //stream data를 넣는 곳. -> 가속도계의 이벤트 값이 여기로 들어옴.
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              // !을 붙여 데이터가 없을 경우
-              return const Center(child: CircularProgressIndicator());
-            }
+      body: Stack(
+        children: [
+          StreamBuilder<AccelerometerEvent>(
+              stream:
+                  accelerometerEventStream(), //stream data를 넣는 곳. -> 가속도계의 이벤트 값이 여기로 들어옴.
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                final event = snapshot.data!;
+                // if 문으로 한번 걸렀기 때문에 분명히 데이터가 있으므로 !
 
-            // 데이터가 잘 들어오는지 찍어보기
-            final event = snapshot.data!; // if 문으로 한번 걸렀기 때문에 분명히 데이터가 있으므로 !
-            List<double> accelerometerValues = [event.x, event.y];
-            // print(accelerometerValues);
-
-            return Positioned(
-              left: centerX,
-              top: centerY,
-              // 컨테이너 위치가 계속 바뀌는게 목표이기 때문에, stream으로 여러 값을 전달해야한다. -> positioned를 감싸기.
+                return Positioned(
+                  left: centerX + event.y,
+                  top: centerY + event.x,
+                  // 컨테이너 위치가 계속 바뀌는게 목표이기 때문에, stream으로 여러 값을 전달해야한다. -> positioned를 감싸기.
+                  child: Container(
+                      decoration: const BoxDecoration(
+                          color: Colors.green, shape: BoxShape.circle),
+                      width: 100,
+                      height: 100),
+                );
+              }),
+          Positioned(
+              top: centerY + 50,
               child: Container(
-                  decoration: const BoxDecoration(
-                      color: Colors.red, shape: BoxShape.circle),
-                  width: 100,
-                  height: 100),
-            );
-          }),
-    ]));
+                width: (centerX + 50) * 2,
+                child: const Divider(color: Colors.red, thickness: 2.0),
+              )),
+          Positioned(
+            top: 0,
+            bottom: 0,
+            child: Container(
+              width: (centerX + 50) * 2,
+              child: const VerticalDivider(color: Colors.red, thickness: 2.0),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
