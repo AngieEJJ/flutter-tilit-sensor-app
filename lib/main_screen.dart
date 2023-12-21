ddd029ee6148ccd13b6aadc636799f370c21e0b7
eunjinjeong <eunjin.jeong.5115@gmail.com>
2023-12-22 00:15:48 +0900

    stack으로 화면 UI 틀 완성

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -10,6 +10,19 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final centerX = MediaQuery.of(context).size.width / 2 - 50;
+    final centerY = MediaQuery.of(context).size.height / 2 - 50;
+    return Scaffold(
+        body: Stack(children: [
+      Positioned(
+        left: centerX,
+        top: centerY,
+        child: Container(
+            decoration:
+                BoxDecoration(color: Colors.red, shape: BoxShape.circle),
+            width: 100,
+            height: 100),
+      ),
+    ]));
   }
 }
