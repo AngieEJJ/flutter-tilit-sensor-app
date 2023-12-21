470a2ef94103ecbb4581e3d03c8e7402634ab8a2
eunjinjeong <eunjin.jeong.5115@gmail.com>
2023-12-22 00:39:21 +0900

    sensor_plus 적용 및 strembuilder 추가

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -14,15 +15,32 @@ class _MainScreenState extends State<MainScreen> {
     final centerY = MediaQuery.of(context).size.height / 2 - 50;
     return Scaffold(
         body: Stack(children: [
-      Positioned(
-        left: centerX,
-        top: centerY,
-        child: Container(
-            decoration:
-                BoxDecoration(color: Colors.red, shape: BoxShape.circle),
-            width: 100,
-            height: 100),
-      ),
+      StreamBuilder<AccelerometerEvent>(
+          //AccelerometerEvent를 받는다.
+          stream:
+              accelerometerEventStream(), //stream data를 넣는 곳. -> 가속도계의 이벤트 값이 여기로 들어옴.
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              // !을 붙여 데이터가 없을 경우
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            // 데이터가 잘 들어오는지 찍어보기
+            final event = snapshot.data!; // if 문으로 한번 걸렀기 때문에 분명히 데이터가 있으므로 !
+            List<double> accelerometerValues = [event.x, event.y];
+            print(accelerometerValues);
+
+            return Positioned(
+              left: centerX,
+              top: centerY,
+              // 컨테이너 위치가 계속 바뀌는게 목표이기 때문에, stream으로 여러 값을 전달해야한다. -> positioned를 감싸기.
+              child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.red, shape: BoxShape.circle),
+                  width: 100,
+                  height: 100),
+            );
+          }),
     ]));
   }
 }
